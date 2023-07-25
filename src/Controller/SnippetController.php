ecec307a104df599371726013f5cf58659ce2409
Marija Havaic <marija.havaic@gmail.com>
2023-07-25 14:43:47 +0200

    Add custom snippet

--- src/Controller/SnippetController.php
+++ src/Controller/SnippetController.php
@@ -2,17 +2,20 @@
 
 namespace App\Controller;
 
+use App\Entity\Snippet;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class SnippetController extends AbstractController
-{
-    #[Route('/snippet/{id}', name: 'app_snippet')]
-    public function index(): Response
+{ 
+    #[Route('/snippet/{id}', name: 'show_code')]
+    public function index(Snippet $snippet, Request $request): Response
     {
+        
         return $this->render('snippet/index.html.twig', [
-            'controller_name' => 'SnippetController',
+            'snippet' => $snippet,
         ]);
     }
 }
