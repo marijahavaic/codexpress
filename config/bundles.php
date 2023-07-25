8edfc54707839f936cc0eca547a283b367050fc8
Marija Havaic <marija.havaic@gmail.com>
2023-07-25 16:58:44 +0200

    Add CSS animations, add Typed; style logout and login

--- config/bundles.php
+++ config/bundles.php
@@ -14,4 +14,7 @@ return [
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     SymfonyCasts\Bundle\VerifyEmail\SymfonyCastsVerifyEmailBundle::class => ['all' => true],
     Knp\Bundle\PaginatorBundle\KnpPaginatorBundle::class => ['all' => true],
+    Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
+    Symfony\UX\Typed\TypedBundle::class => ['all' => true],
+    Symfony\UX\StimulusBundle\StimulusBundle::class => ['all' => true],
 ];
