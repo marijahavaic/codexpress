a1309a10b6173518bf8b95eac5db55792fee2c06
Marija Havaic <marija.havaic@gmail.com>
2023-07-27 09:41:17 +0200

    Finalise frontend apperance

--- src/Controller/PageController.php
+++ src/Controller/PageController.php
@@ -3,38 +3,38 @@
 namespace App\Controller;
 
 use App\Repository\SnippetRepository;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Knp\Component\Pager\PaginatorInterface;
-
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class PageController extends AbstractController
 {
     // Route de la page d'accueil
     #[Route('/', name: 'app_page')]
     public function index(
-        SnippetRepository $snippets,
-        PaginatorInterface $paginator,
-        Request $request
+        SnippetRepository $snippets, // Chargement du repository Snippet
+        PaginatorInterface $paginator, // Chargement de PaginatorInterface
+        Request $request // Chargement de Request
     ): Response
     {
+        // On créer une requête pour récupérer les snippets
         $query = $snippets->findBy(
-            ['isPublished' => true], //pour selectionner
-            ['createdAt' => 'DESC'], // pour trier
-            100 //Pour limiter l'affichage
+            ['isPublished' => true, 'isPublic' => true], // Pour sélectionner les snippets publics et publiés
+            ['createdAt' => 'DESC'], // Pour trier
+            100 // Pour limiter l'affichage
         );
 
         // On utilise le paginator pour paginer les snippets
         $pagination = $paginator->paginate(
-            $query,
-            $request->query->getInt('page', 1), /*page number*/
-            10 /*limit per page*/
+            $query, // Requête contenant les données à paginer
+            $request->query->getInt('page', 1), // Numéro de la page en cours, 1 par défaut
+            9 // Nombre de résultats par page
         );
         
         return $this->render('page/index.html.twig', [
-            'snippets' => $pagination,
+            'snippets' => $pagination
         ]);
     }
 }
--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -19,23 +19,32 @@ class AppFixtures extends Fixture
         $user = new User();
         $user->setEmail('hello@codexpress.fr')
         ->setUsername('codexpress')
-        ->setPassword('$2y$13$4UbZtgjJ2J0JSmY45CZs4uGbUbckq1R.N64JltRbz7JTVpuo3YJzi')
-        ->setRoles(["ROLE_USER"])
+        ->setPassword('$2y$13$4UbZtgjJ2J0JSmY45CZs4uGbUbckq1R.N64JltRbz7JTVpuo3YJzi') // mdp : admin
+        ->setRoles(["ROLE_ADMIN"])
         ->setIsVerified(true)
         ;
 
         // Enregistrement de l'utilisateur en base de données
         $manager->persist($user);
 
+        $snippetContent = '
+        <h1>Mon premier snippet</h1>
+        <p>Voici mon premier snippet créé avec Cod\'Express !</p>
+        <p>Vous pouvez le modifier ou le supprimer depuis votre espace membre.</p>
+        <p>Vous pouvez également le partager avec le lien généré.</p>
+        ';
+
+        $randomBoolean = [true, false];
+
         // Boucle pour créer 200 snippets de test
         for ($i=0; $i < 200; $i++) { 
             $snippet = new Snippet();
             $snippet->setTitle($faker->word(2))
-            ->setContent($faker->text(200))
+            ->setContent($snippetContent)
             ->setUser($user)
             ->setCreatedAt($faker->dateTimeBetween('-7 months'))
-            ->setIsPublished(true)
-            ->setIsPublic(false)
+            ->setIsPublished($randomBoolean[array_rand($randomBoolean)])
+            ->setIsPublic($randomBoolean[array_rand($randomBoolean)])
             ;
 
             $manager->persist($snippet);
